e40c20d31d74b817cd70b22f8169586e0c7c0b19
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2015-10-07 13:15:19 -0700

    Initial commit, 7 October 2015
    Only contains the file patrick_logger.py. But who knows how big this repo will grow?

---
+++ patrick_logger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""patrick_logger.py is a simple multi-level logger for when the
+standard library module is way more than needed. Several of my scripts
+depend on this module.
+
+GPL v3+ at your option. No guarantees or representations of fitness
+or warranties apply. This is free software; you're getting more than
+what you paid for. Nevertheless, I'd love to hear suggestions or
+feedback if anything occurs to you.
+
+http://patrickbrianmooney.nfshost.com/~patrick/
+
+v1, 7 October 2015. 
+""" 
+
+def log_it(message,minimum_level=1):
+    """Add a message to the log if verbosity_level is at least minimum_level.
+    Currently, the log goes to standard output.
+    """
+    if verbosity_level >= 4: # set verbosity to at least 4 to get this message output in the debug log
+        print "\nDEBUGGING: function log_it() called"
+    if verbosity_level >= minimum_level:
+        print(message)
+
