e449bb3c4ccafd9d7d3610676dd5cde6c95a1844
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-01-22 15:02:49 -0800

    22 Jan: documented formats expected for social media clients

--- social_media.py
+++ social_media.py
@@ -9,11 +9,28 @@ authentication constants."""
 from tumblpy import Tumblpy
 import tweepy
 
+# Format for Tumblr clients is:
+# the_client = Tumblpy(
+#    'a string',   # consumer_key
+#    'a string',   # consumer_secret
+#    'a string',   # token_key
+#    'a string'    # token_secret
+#)
+
 def tumblr_text_post(the_client, the_tags, the_title, the_content):
     tumblog_url = the_client.post('user/info')
     tumblog_url = tumblog_url['user']['blogs'][0]['url']
     the_status = the_client.post('post', blog_url=tumblog_url, params={'type': 'text', 'tags': the_tags, 'title': the_title, 'body': the_content})
     return the_status
+
+
+# Format for Twitter clients is:
+# a_client = {
+#    'consumer_key'        : 'a string',
+#    'consumer_secret'     : 'a string',
+#    'access_token'        : 'a string',
+#    'access_token_secret' : 'a string'
+#    }
 
 def post_tweet(the_client, the_tweet):
         auth = tweepy.OAuthHandler(the_client['consumer_key'], the_client['consumer_secret'])
@@ -21,4 +38,4 @@ def post_tweet(the_client, the_tweet):
         api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         status = api.update_status(status=the_tweet)
         return status
-        
+
