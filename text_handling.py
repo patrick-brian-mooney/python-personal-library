c6316ae0c68da27b7db51c3e1dbeb60611e7e9d7
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-11-24 02:10:29 -0800

    23 Nov 2016: added text_handling.py

--- text_handling.py
+++ text_handling.py
@@ -15,8 +15,9 @@ def terminal_width():
 def print_indented(paragraph, each_side=4):
     lines = textwrap.wrap(paragraph, width=terminal_width() - 2*each_side, replace_whitespace=False, expand_tabs=False, drop_whitespace=False)
     for l in lines:
-        print(' ' * each_side + l)
+        l = ' ' * each_side + l.strip()
+        print(l)
 
 if __name__ == "__main__":
-    print("ERROR: text_handling.py is a collection of utilities for other programs to use. It's not itself a program you can run from the command line.") 
+    print_indented("ERROR: text_handling.py is a collection of utilities for other programs to use. It's not itself a program you can run from the command line.") 
     sys.exit(1)
