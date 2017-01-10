f1c93bb5964caa2c655e0c9fadfcf1cda18117c1
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-01-10 03:47:10 -0800

    9 Jan 2017: adding some Twitter functions to social_media.py

--- social_media.py
+++ social_media.py
@@ -8,8 +8,10 @@ in the GitHub repo that contains this unit, because, well, those are my private
 authentication constants.
 """
 
-from tumblpy import Tumblpy
+from tumblpy import Tumblpy     # [sudo] pip[3] install python-tumblpy; https://github.com/michaelhelmick/python-tumblpy
 import tweepy
+
+from patrick_logger import log_it   # https://github.com/patrick-brian-mooney/python-personal-library/blob/master/patrick_logger.py
 
 # Format for Tumblr clients is:
 # the_client = Tumblpy(
@@ -43,3 +45,25 @@ def post_tweet(the_client, the_tweet):
         status = api.update_status(status=the_tweet)
         return status
 
+def post_reply_tweet(text, user_id, tweet_id):
+    """Post a reply tweet. TWEET_ID is the id of the tweet that this tweet is a reply
+    to; the USER_ID is the person to whom we are replying, and the user_id is
+    automatically prepended to the beginning of TEXT before posting.
+
+    Currently does not actually post the tweet, but just prints to stdout.
+    """
+    log_it("INFO: posting tweet: @%s %s  ----  in reply to tweet ID# %d" % (user_id, text, tweet_id))
+    # the_API.update_status("@%s %s" % (user_id, text), in_reply_to_status_id = tweet_id)
+
+def modified_retweet(text, user_id, tweet_id):
+    """Tweet a message about another tweet.
+    """
+    log_it("%s\n\nhttps://twitter.com/%s/status/%s" % (text, user_id, tweet_ID))
+    # the_API.update_status("%s\n\nhttps://twitter.com/%s/status/%s" % (text, user_id, tweet_ID))
+
+def send_DM(text, user):
+    """Send a direct message to another user. Currently, this method is only used to
+    reply to DMs sent by other users. Does not currently do anything.
+    """
+    log_it("DM @%s: %s" % (user, text))
+    # the_API.send_direct_message(user, text)
